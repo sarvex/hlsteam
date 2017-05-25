f36ec05e75f2ca75276f24a5a9b5ab599dc02b8f
Pascal Péridont <pperidont@mtwin.fr>
2017-05-25 12:53:52 +0200

    Added UID.getInt64()

--- steam/UID.hx
+++ steam/UID.hx
@@ -7,6 +7,9 @@ abstract UID(hl.Bytes) {
 	public function getBytes() {
 		return this.toBytes(8);
 	}
+	public function getInt64() : haxe.Int64 {
+		return haxe.Int64.make(this.getI32(4), this.getI32(0));
+	}
 	@:op(a == b) static function __compare( a : UID, b : UID ) {
 		return (cast a : hl.Bytes).compare(0, (cast b : hl.Bytes), 0, 8) == 0;
 	}
